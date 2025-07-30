7258b15341155268c07d21857ed037d266a93a38
Berke Seker <berkeseker.2000@gmail.com>
2025-07-30 19:03:56 +0300

    Navbar ve Footer'ı sabit tutar, değişen sayfa içeriğini <Outlet> ile gösterir.

--- nutriai-final/src/App.jsx
+++ nutriai-final/src/App.jsx
@@ -1,27 +1,19 @@
-// src/App.jsx
-
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Trust from './components/Trust'
-import HowItWorks from './components/HowItWorks'
-import Experts from './components/Experts' 
-import CtaForm from './components/CtaForm'
-import Footer from './components/Footer'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="bg-gray-50">
       <Navbar />
       <main>
-        <Hero />
-        <Trust />
-        <HowItWorks />
-        <Experts /> {/* Yeni bölümü buraya ekledik */}
-        <CtaForm />
+        {/* URL'e göre ilgili sayfa (HomePage, AboutPage vb.) burada gösterilecek */}
+        <Outlet /> 
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
