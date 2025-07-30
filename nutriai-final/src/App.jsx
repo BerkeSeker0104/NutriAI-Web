57c9a4da2481b54a926f1da85001b17da7fa0c79
Berke Seker <berkeseker.2000@gmail.com>
2025-07-30 17:34:56 +0300

    eni bölümü ana sayfamıza ekledim

--- nutriai-final/src/App.jsx
+++ nutriai-final/src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Trust from './components/Trust'
+import HowItWorks from './components/HowItWorks' // 1. Yeni bileşeni import et
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <Trust />
+        <HowItWorks /> {/* 2. Yeni bileşeni buraya ekle */}
       </main>
     </div>
   )
